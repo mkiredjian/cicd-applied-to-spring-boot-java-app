fc9bbaff0b3acc54996da796e5898ab9670bb935
mkiredjian <mosigk@gmail.com>
2019-11-07 15:12:37 +0200

    First Upload

---
+++ src/main/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplication.java
@@ -0,0 +1,22 @@
+package com.cicd.cicdappliedtospringbootjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdAppliedToSpringBootJavaAppApplication {
+
+	@GetMapping("/")
+	public String hello()
+	{
+		return "Hello world from DZONE";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAppliedToSpringBootJavaAppApplication.class, args);
+	}
+
+}
